dc6c5cc27d6259356f067c759ed239372508c6d7
ChandanBose666 <bosechandan21gmail.com>
2022-11-01 03:57:01 +0530

    Optimising the Restaurant Info Card component with a generic view

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,21 +1,28 @@
 import React from "react";
 import styled from "styled-components/native";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import { SvgXml } from "react-native-svg";
 import { Card } from "react-native-paper";
 import { bhojanLogo as logo } from "../../../../assets/bhojanLogo.svg";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Korean Corner",
-    icon = { logo },
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = "Birati",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover
@@ -26,6 +33,25 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       {/* source={{ uri: photos[0] }} */}
       <Info>
         <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map((index) => (
+              <SvgXml key={index} xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            <Spacer variant="left.large" />
+            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            <Spacer variant="left.large" />
+            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+          </SectionEnd>
+        </Section>
+
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
@@ -34,6 +60,23 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
 
 const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
+`;
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const Section = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
 `;
 
 const Address = styled(Text)`
