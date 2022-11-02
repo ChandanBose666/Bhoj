46ae391022a7028b77eea8b9d38850d2944399aa
ChandanBose666 <bosechandan21gmail.com>
2022-11-03 00:56:37 +0530

    Optimised code

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,9 +1,16 @@
 import React from "react";
-import styled from "styled-components/native";
-import { View, Text, Image } from "react-native";
+import { Text, Image } from "react-native";
+import {
+  Section,
+  SectionEnd,
+  Title,
+  Address,
+  Info,
+  RestaurantCard,
+  RestaurantCardCover,
+  Rating,
+} from "./restaurant-info-card-styled-component";
 import { SvgXml } from "react-native-svg";
-import { Card } from "react-native-paper";
-import { bhojanLogo as logo } from "../../../../assets/bhojanLogo.svg";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
 import { Spacer } from "../../../components/spacer/spacer.component";
@@ -51,50 +58,8 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
             <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
           </SectionEnd>
         </Section>
-
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
   );
 };
-
-const Info = styled.View`
-  padding: ${(props) => props.theme.space[3]};
-`;
-const Rating = styled.View`
-  flex-direction: row;
-  padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
-`;
-
-const Section = styled.View`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-`;
-
-const SectionEnd = styled.View`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-end;
-`;
-
-const Address = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.caption};
-`;
-
-const Title = styled.Text`
-  font-size: ${(props) => props.theme.fontSizes.body};
-  font-family: ${(props) => props.theme.fonts.heading};
-  color: ${(props) => props.theme.colors.ui.primary};
-`;
-
-const RestaurantCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-  padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,7 +1,10 @@
 import React from "react";
-import styled from "styled-components";
 import { Searchbar } from "react-native-paper";
-import { StatusBar, SafeAreaView, View } from "react-native";
+import {
+  SafeArea,
+  SearchContainer,
+  RestaurantList,
+} from "./restaurant-screen-styled-components";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
@@ -15,17 +18,3 @@ export const RestaurantsScreen = () => (
     </RestaurantList>
   </SafeArea>
 );
-
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
-
-const SearchContainer = styled(View)`
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const RestaurantList = styled(View)`
-  flex: 1;
-  padding: ${(props) => props.theme.space[3]};
-`;
